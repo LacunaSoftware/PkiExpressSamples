633d15c3c697c605cbec5561b12c63bcbf91857d
Leonardo Pignataro <leopignataro@gmail.com>
2018-02-09 15:40:51 -0200

    Commented out server key samples, which required PKI Express 1.3 (not yet released)

--- PHP/index.php
+++ PHP/index.php
@@ -18,7 +18,7 @@
             <ul>
                 <li><a href="pades-signature.php">Create a signature with a file already on server</a></li>
                 <li><a href="upload.php?goto=pades-signature">Create a signature with a file uploaded by user</a></li>
-                <li><a href="upload.php?goto=pades-signature-server-key">Create a signature using a server key</a></li>
+                <!--<li><a href="upload.php?goto=pades-signature-server-key">Create a signature using a server key</a></li>-->
             </ul>
         </li>
         <li>
@@ -26,14 +26,14 @@
             <ul>
                 <li><a href="cades-signature.php">Create a signature with a file already on server</a></li>
                 <li><a href="upload.php?goto=cades-signature">Create a signature with a file uploaded by user</a></li>
-                <li><a href="upload.php?goto=cades-signature-server-key">Create a signature using a server key</a></li>
+                <!--<li><a href="upload.php?goto=cades-signature-server-key">Create a signature using a server key</a></li>-->
             </ul>
         </li>
         <li>
             XML signature
             <ul>
                 <li><a href="xml-element-signature.php">Create a XML element signature</a></li>
-                <li><a href="xml-element-signature-server-key.php">Create a XML element signature using a server key</a></li>
+                <!--<li><a href="xml-element-signature-server-key.php">Create a XML element signature using a server key</a></li>-->
             </ul>
         </li>
     </ul>
